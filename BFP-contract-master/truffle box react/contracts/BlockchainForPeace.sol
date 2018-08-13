4f4a4c420d4fef6eb3080eb050c80282aa7bb8c1
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2018-08-13 19:46:04 -0400

    made backend better

--- BFP-contract-master/truffle box react/contracts/BlockchainForPeace.sol
+++ BFP-contract-master/truffle box react/contracts/BlockchainForPeace.sol
@@ -21,16 +21,19 @@ contract BlockchainForPeace {
     }
    
     // payable function which auto trasnfers money to charity address, collects the value and increases the total value counter.
-    function () payable public {
+    function fallback() payable public {
+     donors[msg.sender].value = msg.value; 
+     raised += msg.value;
+    }
+    // optional message to be sent with donation, peace message.
+    function messageForPeace(string _message) payable public {
+        require(donors[msg.sender].value != 0);
+        donors[msg.sender].message = _message;
         charity.transfer(msg.value);
         donors[msg.sender].value = msg.value; 
         raised += msg.value;
-    }
-    // optional message to be sent with donation, peace message.
-    function messageForPeace(string _message) public {
-        require(donors[msg.sender].value != 0);
-        donors[msg.sender].message = _message;
+        emit Donate(msg.sender, msg.value, _message);
     }
     
-    event Donate (address indexed from, uint amount);
+    event Donate(address indexed from, uint amount, string message);
 }
