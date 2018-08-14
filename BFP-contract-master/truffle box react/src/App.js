f493a1c7120f40ecae7f8bb2eb76f6046442c571
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2018-08-13 21:00:34 -0400

    things

--- BFP-contract-master/truffle box react/src/App.js
+++ BFP-contract-master/truffle box react/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import BlockchainForPeace from '../build/contracts/BlockchainForPeace.json'
 import getWeb3 from './utils/getWeb3'
+
 
 import SimpleTable from './table/SimpleTable'
 import CustomizedInputs from './TextFields/CustomizedInputs'
@@ -33,7 +35,7 @@ class App extends Component {
       })
 
       // Instantiate contract once web3 provided.
-      this.instantiateContract()
+      
     })
     .catch(() => {
       console.log('Error finding web3.')
@@ -49,29 +51,38 @@ class App extends Component {
      */
 
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    //const simpleStorage = contract(SimpleStorageContract)
+    const BlockchainForPeace = contract(BlockchainForPeace)
+    BlockchainForPeace.setProvider(this.state.web3.currentProvider)
+    //simpleStorage.setProvider(this.state.web3.currentProvider)
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    // var simpleStorageInstance
+    var BlockchainForPeaceInstance
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
+      BlockchainForPeaceInstance.deployed().then((instance) => {
+        BlockchainForPeaceInstance = instance
+        console.log('deployed');
+      //simpleStorage.deployed().then((instance) => {
+        //simpleStorageInstance = instance
+
 
         // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
+        //return simpleStorageInstance.set(5, {from: accounts[0]})
+      //}).then((result) => {
         // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
+        //return simpleStorageInstance.get.call(accounts[0])
+   //  }).then((result) => {
         // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
+       // return this.setState({ storageValue: result.c[0] })
+     })
     })
   }
-
+   _onClick() {
+    this.messageForPeace();
+   }
   render() {
     return (
       <div className="App">
@@ -84,7 +95,7 @@ class App extends Component {
             <div className="pure-u-1-1">
                 
                 <CustomizedInputs />
-               
+                <button onClick={() => this._onClick}/>
                 <SimpleTable />
             </div>
           </div>
